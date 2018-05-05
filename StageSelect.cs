bf0435f5b2749a9496205ca3d78636fde94c0c63
Ishiikotaro145 <38449648+Ishiikotaro145@users.noreply.github.com>
2018-05-05 16:55:57 +0900

    Add files via upload

---
+++ StageSelect.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StageSelect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+
+	public void ButtonTap(int _Stage)
+	{
+		//フェード処理 
+		PlayerPrefs.SetInt("StageSelect",_Stage);
+
+
+
+
+		//フェードが完全に終わったら次のシーンへ
+		Application.LoadLevel("Main");
+
+	}
+}
