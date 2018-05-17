cd05271d85cfbabd5ce681276f33e075c2672b70
BaojunZhou <b-zhou@colopl.co.jp>
2018-05-18 01:42:29 +0900

    Item and wall.

--- Assets/Scripts/GameScript.cs
+++ Assets/Scripts/GameScript.cs
@@ -8,7 +8,7 @@ public class GameScript : MonoBehaviour
 {
     public static GameScript instance;
 
-public Sprite[] TutorImages;
+    public Sprite[] TutorImages;
 
     public GameObject hearts;
     public GameObject Tutor;
@@ -16,6 +16,8 @@ public Sprite[] TutorImages;
     public AudioClip WaveClearA;
     public AudioClip GameOverA;
     public AudioClip StageClearA;
+    public GameObject ItemPrefab;
+    public float ItemTime = 20;
 
 //    public GameObject chargeBar;
     public GameObject tapToStart;
@@ -41,6 +43,8 @@ public Sprite[] TutorImages;
     private float chargeStartTime;
 
     private AudioSource _audioSource;
+
+    private int itemCount = 1;
 //    private Image chargeBarImage;
 
     void Awake()
@@ -70,12 +74,12 @@ public Sprite[] TutorImages;
         }
         else if (PlayerPrefs.GetInt("StageSelect", 0) == 1)
         {
-            Tutor.GetComponent<Image>().sprite=TutorImages[0];
+            Tutor.GetComponent<Image>().sprite = TutorImages[0];
             StartCoroutine("OpenTutor");
         }
         else if (PlayerPrefs.GetInt("StageSelect", 0) == 2)
         {
-            Tutor.GetComponent<Image>().sprite=TutorImages[1];
+            Tutor.GetComponent<Image>().sprite = TutorImages[1];
             StartCoroutine("OpenTutor");
         }
         else PrepareForNextTurn();
@@ -95,7 +99,7 @@ public Sprite[] TutorImages;
         {
             gameOverUI.SetActive(true);
             ChangeBackSprite.instance.StopBGM();
-            _audioSource.PlayOneShot(GameOverA,5);
+            _audioSource.PlayOneShot(GameOverA, 5);
             GuideLines.instance.RemoveAll();
             StartCoroutine("NextScene");
         }
@@ -116,10 +120,10 @@ public Sprite[] TutorImages;
     public void WaveClear()
     {
 //        StageManager.GetInstance.SetWaveClear();  
-        _audioSource.PlayOneShot(WaveClearA,5);
+        _audioSource.PlayOneShot(WaveClearA, 5);
         speed = playerInstance.GetCurrentSpeed();
         playerInstance.StopAllCoroutine();
-        Destroy(playerInstance.gameObject); 
+        Destroy(playerInstance.gameObject);
 //        StageManager.GetInstance.RestorePassableImmediately();
         GuideLines.instance.RemoveAll();
 //        StoveScript.instance.Reset();
@@ -127,7 +131,8 @@ public Sprite[] TutorImages;
             Instantiate(player, new Vector2(0, startYusyaPositionY),
                     Quaternion.FromToRotation(Vector3.right, Vector3.up))
                 .GetComponent<BallScript>();
-        Time.timeScale=1;
+        Time.timeScale = 1;
+        StopAllCoroutines();
     }
 
     private void Update()
@@ -167,20 +172,21 @@ public Sprite[] TutorImages;
     }
 
     IEnumerator OpenTutor()
-    { 
+    {
         yield return new WaitForSeconds(.5f);
         Tutor.SetActive(true);
     }
-    
+
     IEnumerator CloseTutor()
     {
         Tutor.SetActive(false);
         yield return new WaitForSeconds(.5f);
         PrepareForNextTurn();
     }
+
     public void PrepareForNextTurn()
     {
-        Time.timeScale=1;
+        Time.timeScale = 1;
         tapToStart.SetActive(true);
         readyToStart = true;
         isMouseDown = false;
@@ -190,9 +196,10 @@ public Sprite[] TutorImages;
     {
         playerInstance.GameStop();
         ChangeBackSprite.instance.StopBGM();
-        _audioSource.PlayOneShot(StageClearA,2);
+        _audioSource.PlayOneShot(StageClearA, 2);
         clearUI.active = true;
-        gameClear = true; 
+        gameClear = true;
+        StopAllCoroutines();
 //        StartCoroutine("NextScene");
     }
 
@@ -209,6 +216,10 @@ public Sprite[] TutorImages;
 //        playerInstance = Instantiate(player, new Vector2(0, -2f), Quaternion.identity).GetComponent<BallScript>();
         playerInstance.GameStart(speed);
         StoveScript.instance.GameStart();
+        if (itemCount == 0) yield break;
+        yield return new WaitForSeconds(ItemTime);
+        Instantiate(ItemPrefab, new Vector2(Random.value * 4 - 2, Random.value * 4 - 2), Quaternion.identity);
+        itemCount--;
     }
 
     public void Pause(bool isPause)
@@ -229,17 +240,21 @@ public Sprite[] TutorImages;
     {
         Time.timeScale = 1;
         SceneManager.LoadScene("Main");
+        StopAllCoroutines();
     }
 
-    public void NextStage(){
-        PlayerPrefs.SetInt("StageSelect",PlayerPrefs.GetInt("StageSelect", 0)+1);
+    public void NextStage()
+    {
+        PlayerPrefs.SetInt("StageSelect", PlayerPrefs.GetInt("StageSelect", 0) + 1);
         Time.timeScale = 1;
         SceneManager.LoadScene("Main");
+        StopAllCoroutines();
     }
 
     public void GoToMenu()
     {
         Time.timeScale = 1;
         SceneManager.LoadScene("StageSelect");
+        StopAllCoroutines();
     }
 }
